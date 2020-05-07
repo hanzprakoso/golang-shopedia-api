7969068c5d0ece09b9c2d2de1687ac61c10b0916
M. Hanan Brian Prakoso <hanzprakoso27@gmail.com>
2020-05-07 22:03:13 +0700

    add some packages

--- controllers/productController.go
+++ controllers/productController.go
@@ -1,22 +1,93 @@
 package controllers
 
 import (
-
+	"fmt"
+	"net/http"
+	"strings"
+	"strconv"
 	"shopedia/models"
-	// "github.com/labstack/echo"
+	"github.com/labstack/echo"
+	"github.com/jinzhu/gorm"
 )
-type data_result struct{
-	Data products_result `json:"data"`
+
+func product(db *gorm.DB, w string, p ...string) models.Product {
+	var (
+		P models.Product
+		C models.Category
+		CD models.CategoryDetails 
+	)
+	where := fmt.Sprintf("%v", w)
+	param := p
+	db.Preload("Category").Preload("Merchant").Preload("WeightUnit").Find(&P, where, param )
+
+	id := P.Category.ParentID
+	db.Where("CategoryID = ?", id).Find(&CD)
+	P.Category.CategoryDetails = append(P.Category.CategoryDetails, CD)
+	var i = 0
+	for {
+		CD = models.CategoryDetails{}
+		if id := P.Category.CategoryDetails[i].ParentID; id != 0{
+			db.Model(&models.Category{}).Raw("SELECT * FROM categories WHERE CategoryID = ?", id).Scan(&CD)
+			P.Category.CategoryDetails = append(P.Category.CategoryDetails, CD)
+		}else{
+			break
+		}
+		i++
+	}
+	fmt.Printf("%+v\n", &C)
+	return P
 }
-type products_result struct {
-	Products     []models.Product `json:"products"`
-	Product 	models.Product
-	Message   string `json:"message"`
-	Error     error  `json:"error"`
-	IsSuccess bool   `json:"is_success"`
+func (idb *InDB) GetProducts(e echo.Context) error {
+	db := idb.DB
+	db.LogMode(true)
+	//fungsi produk dengan parameter kedua WHERE dan ketiga parameternya
+	//product(db, "1", "1") => ...WHERE "1" = "1"q v
+	return e.JSON(http.StatusOK, product(db, "1 = ?", "1"))
+} 
+
+func (idb *InDB) GetProductsByMerchantID(e echo.Context) error {
+	db := idb.DB
+	db.LogMode(true)
+	MID := e.Param("merchant_id")
+	//fungsi produk dengan parameter kedua WHERE dan ketiga parameternya
+	//product(db, "1", "1") => ...WHERE "1" = "1"q v
+	return e.JSON(http.StatusOK, product(db, "MerchantID = ?", MID))
+} 
+
+func (idb *InDB) GetProductsByMerchantName(e echo.Context) error {
+	db := idb.DB
+	MN := e.Param("merchant_name")
+	var m models.Merchant
+	
+	db.LogMode(true)
+	MN = strings.Replace(MN, "-", " ", -1)  //replace all stripes with spaces
+
+	//fungsi produk dengan parameter kedua WHERE dan ketiga parameternya
+	//product(db, "1", "1") => ...WHERE "1" = "1"q v
+	db.Select("MerchantID").Find(&m, "Name = ?", MN)
+	return e.JSON(http.StatusOK, product(db, "MerchantID = ?", strconv.Itoa(m.MerchantID)))
+} 
+
+func (idb *InDB) GetProductByBothNames(e echo.Context) error {
+	db := idb.DB
+	MN := e.Param("merchant_name")
+	PD := e.Param("product_domain")
+	var m models.Merchant
+	
+	db.LogMode(true)
+	MN = strings.Replace(MN, "-", " ", -1)  //replace all stripes with spaces
+
+	//fungsi produk dengan parameter kedua WHERE dan ketiga parameternya
+	//product(db, "1", "1") => ...WHERE "1" = "1"q v
+	db.Select("MerchantID").Find(&m, "Name = ?", MN)
+	return e.JSON(http.StatusOK, product(db, fmt.Sprintf("MerchantID = %v AND Domain = ?", strconv.Itoa(m.MerchantID)), PD))
 }
 
-// func (idb *InDB) getProductByShop(e *echo.Context) {
-// 	db := idb.DB
-	
-// }
+func (idb *InDB) Category(e echo.Context) error {
+	db := idb.DB
+	var c models.Category
+	db.LogMode(true)
+
+	db.Preload("Parents").Find(&c)
+	return e.JSON(http.StatusOK, c)
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,17 @@ go 1.13
 
 require (
 	github.com/alexedwards/argon2id v0.0.0-20200420065805-90c52fcb498a // indirect
+	github.com/cosmtrek/air v1.12.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fatih/color v1.9.0 // indirect
 	github.com/guregu/null v4.0.0+incompatible
+	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/jinzhu/gorm v1.9.12
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
 	gitlab.com/labstack/echo v3.3.10+incompatible // indirect
 	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
 )
